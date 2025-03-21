3c97c30f1a0937bdf98f49cf0cc900d6a936dfed
John-Warosa <elias.mat100@gmail.com>
2025-03-21 13:26:37 +0100

    color can now be rendered

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include "mincpu/cpu.h"
+#include "raylib.h"
 #include "render/render.h"
+#include <stdio.h>
 
 #define WIDTH 320
 #define HEIGHT 320
 #define FPS 60
 
 int main(int argc, char **argv) {
-  uint8_t vram[256] = {0};
-
   render_init(WIDTH, HEIGHT, FPS);
-  render_screen(vram);
+
+  uint8_t vram[256];
+
+  for (size_t i = 0; i < 256; ++i) {
+    vram[i] = i;
+  }
+
+  while (!WindowShouldClose()) {
+    render_screen(vram);
+  }
 }
--- src/render/render.c
+++ src/render/render.c
@@ -1,6 +1,9 @@
 #include "render/render.h"
 #include "raylib.h"
-#include <stdio.h>
+
+#define RGB_MASK_RED 224
+#define RGB_MASK_GREEN 28
+#define RGB_MASK_BLUE 3
 
 static struct {
   // rendering constants
@@ -13,6 +16,11 @@ static struct {
   Texture2D tex;
 } RC;
 
+static void update_pixels(uint8_t vram[]);
+static inline uint8_t get_rgb_red(uint8_t num);
+static inline uint8_t get_rgb_green(uint8_t num);
+static inline uint8_t get_rgb_blue(uint8_t num);
+
 void render_init(size_t width, size_t height, size_t fps) {
   InitWindow(width, height, "Minimal cycle-accurate CPU");
 
@@ -24,6 +32,7 @@ void render_init(size_t width, size_t height, size_t fps) {
 }
 
 void render_screen(uint8_t vram[]) {
+  update_pixels(vram);
   UpdateTexture(RC.tex, RC.img.data);
 
   BeginDrawing();
@@ -31,4 +40,27 @@ void render_screen(uint8_t vram[]) {
   DrawTextureEx(RC.tex, (Vector2){0, 0}, 0, 10, WHITE);
 
   EndDrawing();
+}
+#include <stdio.h>
+static void update_pixels(uint8_t vram[]) {
+  static Color *pixels;
+  pixels = (Color *)RC.img.data;
+
+  for (size_t i = 0; i < 256; ++i) {
+    uint8_t rgb = vram[i];
+    pixels[i] =
+        (Color){get_rgb_red(rgb), get_rgb_green(rgb), get_rgb_blue(rgb), 255};
+  }
+}
+
+static inline uint8_t get_rgb_red(uint8_t num) {
+  return ((num & RGB_MASK_RED) >> 5) * 32;
+}
+
+static inline uint8_t get_rgb_green(uint8_t num) {
+  return ((num & RGB_MASK_GREEN) >> 2) * 32;
+}
+
+static inline uint8_t get_rgb_blue(uint8_t num) {
+  return (num & RGB_MASK_BLUE) * 64;
 }
